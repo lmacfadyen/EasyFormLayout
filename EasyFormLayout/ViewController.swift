2a9b57b09e52a767d11646dc0583001f7e5efd8c
Lawrence F MacFadyen <larry.macfadyen@gmail.com>
2017-07-21 11:35:30 -0400

    Initial Commit

---
+++ EasyFormLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasyFormLayout
+//
+//  Created by Lawrence F MacFadyen on 2017-07-21.
+//  Copyright © 2017 Lawrence F MacFadyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
